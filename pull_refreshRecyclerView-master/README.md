5088da111073c4e88e73ced999e04916d4e67c8b
weills <zhwe70@qq.com>
2015-11-14 00:15:56 +0800

    Signed-off-by: weills <zhwe70@qq.com> main

---
+++ pull_refreshRecyclerView-master/README.md
@@ -0,0 +1,32 @@
+# PullLoadMoreRecyclerView
+RecyclerView实现下拉刷新和上拉加载更多以及瀑布流效果
+
+
+# 使用方法
+build.gradle文件
+```java
+dependencies {
+  compile 'com.wuxiaolong.pullloadmorerecyclerview:library:1.0.1'
+}
+```
+设置线性布局
+```java
+ mPullLoadMoreRecyclerView = (PullLoadMoreRecyclerView) view.findViewById(R.id.pullLoadMoreRecyclerView);
+ mPullLoadMoreRecyclerView.setLinearLayout();
+```
+设置网格布局
+```java
+ mPullLoadMoreRecyclerView.setGridLayout(2);//参数为列数
+```
+设置交错网格布局，即瀑布流效果
+```java
+ mPullLoadMoreRecyclerView.setStaggeredGridLayout(2);//参数为列数
+```
+下拉刷新
+```java
+mPullLoadMoreRecyclerView.setRefreshing(true);
+```
+刷新结束
+```java
+mPullLoadMoreRecyclerView.setPullLoadMoreCompleted();
+```
---
+++ pull_refreshRecyclerView-master/app/src/main/java/com/wuxiaolong/pullloadmorerecyclerviewsample/MainActivity.java
@@ -0,0 +1,106 @@
+package com.wuxiaolong.pullloadmorerecyclerviewsample;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        // 设置ViewPager的数据等
+        tabLayout.setupWithViewPager(viewPager);
+//        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);//适合很多tab
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);//tab均分,适合少的tab
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+
+        Fragment newfragment = new FristFragment();
+        Bundle data = new Bundle();
+        data.putInt("id", 0);
+        newfragment.setArguments(data);
+        adapter.addFrag(newfragment, "Frist");
+
+        newfragment = new SecondFragment();
+        data = new Bundle();
+        data.putInt("id", 1);
+        newfragment.setArguments(data);
+        adapter.addFrag(newfragment, "Second");
+
+
+        newfragment = new ThirdFragment();
+        data = new Bundle();
+        data.putInt("id", 2);
+        newfragment.setArguments(data);
+        adapter.addFrag(newfragment, "Third");
+
+        viewPager.setAdapter(adapter);
+
+        viewPager.setOffscreenPageLimit(3);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return true;
+    }
+}
